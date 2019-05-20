d21e75848a60ad94a894d8b860ddd4ea817b19b3
chenxixian <chenxixian@gmail.com>
2019-05-20 08:04:41 +0800

    更新 'setup.sh'

--- setup.sh
+++ setup.sh
@@ -3,6 +3,13 @@ git clone https://github.com/helloxz/imgurl.git
 cd imgurl
 chmod 777 data
 chmod 777 imgs
+
 sed -i 's/build: ./# build: ./g'  docker-compose.yml
+#Mac: 注释上一句，取消注释下一句
+#sed -i '' 's/build: ./# build: ./g'  docker-compose.yml
+
 sed -i 's/8080:80/80:80/g'  docker-compose.yml
+#Mac: 注释上一句，取消注释下一句
+#sed -i '' 's/8080:80/80:80/g'  docker-compose.yml
+
 docker-compose up -d
