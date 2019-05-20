e48e169c6c019397794cfe533aecc37b80e35756
chenxixian <chenxixian@gmail.com>
2019-05-20 11:00:49 +0800

    更新 'build.sh'

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 git clone https://github.com/helloxz/imgurl.git
 cd imgurl
-docker build -t helloxz/imgurl:2.0 .
-docker save -o  helloxz-imgurl.tar helloxz/imgurl:2.0
+docker build -t zqiannnn/imgurl:2.0 .
+docker save -o  helloxz-imgurl.tar zqiannnn/imgurl:2.0
